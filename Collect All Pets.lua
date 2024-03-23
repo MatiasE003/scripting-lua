2822367d0bcbfb598c049c71cb2701c354e20cee
MatiasE003 <105245263+MatiasE003@users.noreply.github.com>
2024-03-23 02:41:48 -0300

    Update Collect All Pets.lua
    Add Library UI

--- Collect All Pets.lua
+++ Collect All Pets.lua
@@ -1,27 +1,20 @@
 local RunService = game:GetService("RunService")
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local player = game.Players.LocalPlayer
-local char = player.Character
-
 local Areas = game.Workspace.Areas:GetChildren()
 
 getgenv().ClaimQuest = true
-getgenv().Hatch = false
-
-local Magnet = 999999
-local EggSpeed = 999999
-
-player.Magnet.Value = Magnet
-player.HatchSpeedTier.Value = EggSpeed
-player.EnableEggHatchFade.Value = false
+getgenv().Hatch = true
 
 local function ClaimQuest()
 	game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimQuestReward"):FireServer()
 end
 
-local function Hatch()
+--{{ Temporalmente sin uso
+local function Hatch(egg)
 	local args = {
-		[1] = 3
+		[1] = egg
 		--1: common
 		--2: uncommon
 		--3: rare
@@ -32,7 +25,7 @@ local function Hatch()
 
 	game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyEgg"):FireServer(unpack(args))
 end
-
+}}--
 local function getQuestArea()
 	return player.QuestArea.Value
 end
@@ -48,15 +41,69 @@ local function sendPetsToQuest()
 
 end
 
-RunService.Heartbeat:Connect(function()
-	
-	sendPetsToQuest()
-	
-	if getgenv().ClaimQuest then
-		ClaimQuest()
-	end
-	
-	if getgenv().Hatch then
-		Hatch()
-	end
-end)
+local Window = Rayfield:CreateWindow({
+	Name = "Collect All Pets",
+	LoadingTitle = "Maplexito",
+	LoadingSubtitle = "",
+	ConfigurationSaving = {
+		Enabled = true,
+		FolderName = "MaplexHub",
+		FileName = "Collect All Pets"
+	},
+	Discord = {
+		Enabled = false,
+		Invite = "",
+		RememberJoins = true
+	},
+	KeySystem = false,
+	KeySettings = {
+		Title = "Untitled",
+		Subtitle = "Key System",
+		Note = "No method of obtaining the key is provided",
+		FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+		SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+		GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+		Key = {"CamilaBella"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+	}
+})
+
+local Main = Window:CreateTab("Main", 4483362458)
+
+local AutoQuest = Main:CreateToggle({
+	Name = "Auto quest",
+	CurrentValue = false,
+	Flag = "AutoQuest",
+	Callback = function(value)
+		shared["AutoQuest"] = value
+		while shared["AutoQuest"] do
+			task.wait()
+			if not shared["AutoQuest"] then
+				break
+			end
+			sendPetsToQuest()
+		end
+	end,
+})
+
+local AutoClaim = Main:CreateToggle({
+	Name = "Auto claim",
+	CurrentValue = false,
+	Flag = "AutoClaim",
+	Callback = function(value)
+		shared["AutoClaim"] = value
+		while shared["AutoClaim"] do
+			task.wait()
+			if not shared["AutoClaim"] then
+				break
+			end
+			ClaimQuest()
+		end
+	end,
+})
+
+local MaxMagnet = Main:CreateButton({
+	Name = "Magnet max range",
+	Callback = function()
+		player.Magnet.Value = 9999999
+	end,
+})
