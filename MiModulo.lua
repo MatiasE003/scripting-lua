6e406a2cf5367478e4afe6df4df4483283f9a368
MatiasE003 <105245263+MatiasE003@users.noreply.github.com>
2024-03-26 19:48:22 -0300

    create modulo de prueba

---
+++ MiModulo.lua
@@ -0,0 +1,11 @@
+local MiModulo = {}
+
+function MiModulo.saludar()
+    print("Holaaaa desde el ModuleScript")
+end
+
+function MiModulo.despedida()
+    print("Adios desde el ModuleScript")
+end
+
+return MiModulo
