fa4575f62b95ecf206f3acb58d6d585950dae810
MatiasE003 <gamermaplex@gmail.com>
2024-04-06 20:42:19 -0300

    commit

---
+++ HubLoader.lua
@@ -0,0 +1,9 @@
+if game.PlaceId == 8884433153 then
+    loadstring(('https://pastebin.com/raw/WpnMWLTF'):HttpGet(game))()
+elseif game.PlaceId == 14350413280 then
+	loadstring(('https://pastebin.com/raw/xxUw7UGe'):HttpGet(game))()
+elseif game.PlaceId == 15055025587 then
+	loadstring(('https://pastebin.com/raw/jMnEJVPV'):HttpGet(game))()
+else
+	error("Este juego no está en la lista de scripts.")
+end
